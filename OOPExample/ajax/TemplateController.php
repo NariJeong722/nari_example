83b8b3ee15ad026f8b28304235cc67cd9800933d
user <user@user-PC>
2015-03-09 12:18:00 +0900

    load

---
+++ OOPExample/ajax/TemplateController.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<tr>";
+echo "<td>Nari</td>";
+echo "<td>정나리</td>";
+echo "<td>26</td>";
+echo "<td>nari@email.com</td>";		
+echo "</tr>";
+
+?>
--- OOPExample/templateIndex.php
+++ OOPExample/templateIndex.php
@@ -2,37 +2,44 @@
 <head></head>
 <body>
 
-<form action="" method="post">
-	<div id="searchDiv">
-		<select id="search1">
-			<option value="0"> 검색방법 </option>
-			<option value="equal"> = </option>
-			<option value="like">LIKE</option>
-		</select>
-		<select id="search2">
-			<option value="0"> 검색컬럼 </option>
-			<option value="name">이름</option>
-			<option value="id">아이디</option>
-		</select>
-		<input type="text" name="search3" id="search3">
-		<input type="submit" name="searchBtn" id="searchBtn" value="검색">
-	</div>
-	<br>
-	<table border="1" width="800">
-	<thead>
-		<tr align="center">
-			<td>아이디</td>
-			<td>이름</td>
-			<td>나이</td>
-			<td>이메일</td>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-		</tr>
-	</tbody>
-	</table>
-</form>
-
+	<form action="" method="post">
+		<div id="searchDiv">
+			<select id="search1">
+				<option value="0"> 검색방법 </option>
+				<option value="equal"> = </option>
+				<option value="like">LIKE</option>
+			</select>
+			<select id="search2">
+				<option value="0"> 검색컬럼 </option>
+				<option value="name">이름</option>
+				<option value="id">아이디</option>
+			</select>
+				<input type="text" name="search3" id="search3">
+				<input type="submit" name="searchBtn" id="searchBtn" value="검색">
+		</div>
+		<br>
+		<table border="1" width="800">
+			<thead>
+				<tr align="center">
+					<td>아이디</td>
+					<td>이름</td>
+					<td>나이</td>
+					<td>이메일</td>
+				</tr>
+			</thead>
+		
+			<tbody id="id">
+			</tbody>
+		
+		</table>
+	</form>
+	
+	 <script src="//code.jquery.com/jquery-1.10.2.js"></script>	
+	 <script type="text/javascript">
+		$(document).ready(function(){
+			$('tbody').load("/Example/OOPExample/ajax/TemplateController.php");
+		});
+	</script>	
+	
 </body>
 </html>
