6561b63eb9ac2feb290cf9a9b8bd5dc534aab5c9
user <user@user-PC>
2015-03-11 17:32:38 +0900

    0311

--- OOPExample/tpl/templateExample.tpl.php
+++ OOPExample/tpl/templateExample.tpl.php
@@ -1,5 +1,5 @@
 <?php
-header ( 'Content-Type: text/html; charset=utf-8' );
+header ('Content-Type: text/html; charset=utf-8');
 echo "<div>";
 echo "<div><table border='1' width='800'>";
 echo "<tr align='center'>";
